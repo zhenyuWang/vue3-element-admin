ed9a0d5ec88d97bb968e894414657db56365bbc8
ZhenyuWang <13641039885@163.com>
2021-10-15 21:26:12 +0800

    feat: 添加404.vue

---
+++ src/views/404.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>This is NotFund</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NotFund",
+});
+</script>
