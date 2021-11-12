0614535feea07cea591e14208d781f55d045f6df
ZhenyuWang <13641039885@163.com>
2021-11-12 12:28:24 +0800

    chore: 404页面优化

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>404</div>
+  <div class="not_fund flex flex_center flex_direction_column">
+    <div class="logo"></div>
+    <p class="pb_20">您访问的页面不存在</p>
+    <el-button type="primary" @click="$router.push({ name: 'Home' })"
+      >返回首页</el-button
+    >
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
@@ -8,3 +14,13 @@ export default defineComponent({
   components: {},
 });
 </script>
+<style lang="scss" scoped>
+.not_fund {
+  .logo {
+    width: 700px;
+    height: 300px;
+    margin: 200px auto 50px;
+    background: url("@/assets/img/404.jpg");
+  }
+}
+</style>
